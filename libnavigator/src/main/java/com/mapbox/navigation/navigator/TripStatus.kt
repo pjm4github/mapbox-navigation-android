14fe5e78106bc4d5051bfe1dc4ebf93e222a7055
Yury Kanetski <yury.kanetski@mapbox.com>
2020-04-13 14:30:14 +0300

    libnavigator: remaining docs

--- libnavigator/src/main/java/com/mapbox/navigation/navigator/TripStatus.kt
+++ libnavigator/src/main/java/com/mapbox/navigation/navigator/TripStatus.kt
@@ -3,6 +3,16 @@ package com.mapbox.navigation.navigator
 import android.location.Location
 import com.mapbox.navigation.base.trip.model.RouteProgress
 
+/**
+ * State of trip at particular timestamp
+ *
+ * @param enhancedLocation user's location
+ * @param keyPoints list of predicted locations. Might be empty
+ * @param routeProgress [RouteProgress] is progress information
+ * @param offRoute *true* if user is off-route, *false* otherwise
+ *
+ * @see [MapboxNativeNavigator.getStatus]
+ */
 data class TripStatus(
     val enhancedLocation: Location,
     val keyPoints: List<Location>,
